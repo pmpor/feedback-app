bad944176bb696849c300b6ce6df76af2db806d5
Phillip <phillipmporter@gmail.com>
2018-05-25 16:42:22 -0400

    Fixed route handler again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => { hello:'friend' });
+app.get('/', (req, res) => res.send({ hello:'friend' }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
